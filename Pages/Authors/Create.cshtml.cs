96c7aab15aa53522e96ee7d157504c2171e76eb6
vskachko <vitalii.skachkov@t-systems.ru>
2019-02-25 17:08:24 +0300

    'Create author' function implemented

--- Pages/Authors/Create.cshtml.cs
+++ Pages/Authors/Create.cshtml.cs
@@ -2,16 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookManager.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookManager.Pages.Authors
 {
     public class CreateModel : PageModel
     {
+        readonly BookContext db;
+
+        public CreateModel(BookContext ctx)
+        {
+            db = ctx;
+            db.Database.EnsureCreated();
+        }
         public void OnGet()
         {
 
+        }
+
+        [BindProperty]
+        public Author Author { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(Author author)
+        {
+            if (ModelState.IsValid && !string.IsNullOrWhiteSpace(author.LastName))
+            {
+                try { 
+                db.Authors.Add(author);
+                await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    ViewData["Error"] = $"UpdateConcurrencyException: {ex.Message}";
+                    return RedirectToPage("./Index");
+                }
+            }
+            return RedirectToPage("./Index");
         }
     }
 }
--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -14,6 +14,12 @@ namespace BookManager.Pages.Books
         readonly BookContext db;
         readonly int pageSize;
         public PaginatedList<Book> Books { get; set; }
+        public string Sort { get; set; }
+        public string IdSort { get; set; }
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public string Filter { get; set; }
+
 
         public IndexModel(BookContext ctx)
         {
@@ -28,7 +34,30 @@ namespace BookManager.Pages.Books
         public Book Book { get; set; }
             public async Task OnGetAsync(string sort, string filter, string search, int? pageID)
         {
+            if (search != null)
+                pageID = 1;
+            else
+                search = filter;
+            Filter = search;
+
+
+            Sort = sort;
+            IdSort = string.IsNullOrEmpty(sort) ? "idDesc" : "";
+            TitleSort = string.IsNullOrEmpty(sort) ? "titleDesc" : "";
+            AuthorSort = string.IsNullOrEmpty(sort) ? "authorDesc" : "";
+
             var books = db.Books.Include("Author").AsQueryable();
+            //Books = await PaginatedList<Book>.CreateAsync(books, pageID ?? 1, pageSize);
+            switch (sort)
+            {
+                case "idDesc": books = books.OrderByDescending(x => x.Id); break;
+                case "titleDesc": books = books.OrderByDescending(x => x.Title); break;
+                case "authorDesc": books = books.OrderByDescending(x => x.Author.FullName()); break;
+                default: books = books.OrderBy(x => x.Id); break;
+            }
+			if (!string.IsNullOrEmpty(search))
+                books = books.Where(x => x.Title.Contains(search));
+
             Books = await PaginatedList<Book>.CreateAsync(books, pageID ?? 1, pageSize);
         }
 
